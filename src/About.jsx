8b396470e6b61a04a28ac4061b133af1d63fed34
Skyler Mansfield <skyler.mansfield.21@gmail.com>
2026-07-31 00:05:15 +0100

    Add routing with /about page.

---
+++ src/About.jsx
@@ -0,0 +1,48 @@
+import { React, useCallback, useEffect, useRef, useState } from 'react'
+
+const historical_versions = {
+    "0.1.0": {
+        description: "Initial release with basic chat functionality.",
+        cid: "QmPPCyiB4rBJp2mSDzErmcy1PdRzNqsedJex6VMMYWzxLr"
+    }
+}
+
+// Decide if this is being accessed from a local server or an IPFS web-gateway.
+
+const DEFAULT_GATEWAY = "https://inbrowser.link"
+
+// check if current host is CID.ipfs.xxx or CID.ipns.xxx and extract the gateway from the host if so, otherwise use the default gateway
+
+const getGateway = () => {
+    const host = window.location.host
+    const parts = host.split('.')
+    if (parts.length >= 3 && (parts[1] === 'ipfs' || parts[1] === 'ipns')) {
+        return `${window.location.protocol}//${parts.slice(2).join('.')}:${window.location.port}`
+    }
+    return DEFAULT_GATEWAY
+}
+
+function getGatewayURL(cid) {
+    const gateway = getGateway()
+    return `${gateway}/ipfs/${cid}`
+}
+
+function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <h2>IPFS Helia/Libp2p chat app</h2>
+            <p>Written by <a href="https://github.com/Skyler84" target="_blank" rel="noopener noreferrer">Skyler84</a></p>
+            <h2>Historical Versions</h2>
+            <ul>
+                {Object.entries(historical_versions).map(([version, info]) => (
+                    <li key={version}>
+                        <strong>{version}</strong>: {info.description} (CID: <a href={getGatewayURL(info.cid)} target="_blank" rel="noopener noreferrer">{info.cid}</a>)
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default About
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route} from 'react-router'
+import About from './About'
 import App from './App'
 import './index.css'
 import { HeliaProvider } from '@/provider/HeliaProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HeliaProvider>
+  <BrowserRouter>
+  {/* <React.StrictMode> */}
+  <Routes>
+    <Route path="/" element={<HeliaProvider><App /></HeliaProvider>} />
+    <Route path="/about" element={<About />} />
+  </Routes>
+    {/* <HeliaProvider>
       <App />
-    </HeliaProvider>
-  </React.StrictMode>
+    </HeliaProvider> */}
+  {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
