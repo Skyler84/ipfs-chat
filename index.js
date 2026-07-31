d6ce46637eb87c00d88e3bda628f2220a668e8dc
Skyler Mansfield <skyler.mansfield@touchnetix.com>
2026-07-31 16:56:58 +0100

    something?

---
+++ index.js
@@ -0,0 +1,9 @@
+// Fetch settings from local storage. If not present, redirect to the /setup/ page
+try {
+    const settings = JSON.parse(localStorage.getItem('settings'))
+    if (!settings) throw new Error('Settings not found')
+} catch (e) {
+    window.location.href = '/setup/'
+}
+
+const ipfs = window.KuboRpcClient.create( { host: '127.0.0.1', port: 5002})
