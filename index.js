7dcf306ff08eea8424fae85dc2d0eff6b00329e0
Skyler Mansfield <skyler.mansfield@touchnetix.com>
2026-07-31 17:01:11 +0100

    fix redierct

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ try {
     const settings = JSON.parse(localStorage.getItem('settings'))
     if (!settings) throw new Error('Settings not found')
 } catch (e) {
-    window.location.href = '/setup/'
+    window.location.href = '/options/options.html'
 }
 
 const ipfs = window.KuboRpcClient.create( { host: '127.0.0.1', port: 5002})
